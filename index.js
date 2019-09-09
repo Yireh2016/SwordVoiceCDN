3a49aa76c331a0938725a74a6dd90c4bd0f59812
yireh2016 <jainer.calvetti@gmail.com>
2019-09-09 17:44:35 -0400

    adding compression

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cdn = require("./api/cdn");
 const cors = require("cors");
+const compression = require("compression");
 require("dotenv").config();
 const server = express();
 
 //midlewares
 server.use(cors()); //cors enable only dev
+server.use(compression());
 server.use(express.json({ limit: "50mb", extended: true })); //body parser
 
 server.use(express.static("uploads/")); //static files
